1d9074cdd23eb4e948b2574c2049813a75f5b147
Subreena <subreena20@gmail.com>
2021-01-05 20:21:05 +0600

    slight update

--- src/Component/Blog/Blog.js
+++ src/Component/Blog/Blog.js
@@ -59,7 +59,7 @@ const Blog = () => {
               <img src={prb} className="card-img-top" alt="10 things I learned about JavaScript" />
               <div className="card-body">
                 <h5 className="card-title">Problem Solving with JavaScript</h5>
-                <p className="text-muted"><small> <i class="fa fa-user" aria-hidden="true"></i> Subreena  |  <i class="fa fa-calendar" aria-hidden="true"></i> Soon </small></p>
+                <p className="text-muted"><small> <i class="fa fa-user" aria-hidden="true"></i> Subreena  |  <i class="fa fa-calendar" aria-hidden="true"></i> November 5, 2020 </small></p>
                 <p className="card-text">
                 Today I am going to focus on solving problems with JavaScript. I am going from easy to difficult. Problem solving with JavaScript is alot fun and can help you brain storm. I will be using ES6 for problem 
                 </p>
@@ -72,7 +72,7 @@ const Blog = () => {
               <img src={webDevImg} className="card-img-top" alt="How to be a Full Stack Web Developer" />
               <div className="card-body">
                 <h5 className="card-title">How to be a Full Stack Web Developer</h5>
-                <p className="text-muted"><small> <i class="fa fa-user" aria-hidden="true"></i> Subreena  |  <i class="fa fa-calendar" aria-hidden="true"></i> October 20 </small></p>
+                <p className="text-muted"><small> <i class="fa fa-user" aria-hidden="true"></i> Subreena  |  <i class="fa fa-calendar" aria-hidden="true"></i> October 20, 2020 </small></p>
                 <p className="card-text">Web development is one of the most trending proffesions in this fast track century. It has so many amazing benefits that infact it lures people </p>
                 <a href="https://medium.com/@subreena20/how-to-be-a-full-stack-web-developer-290ecab2c550"
                  className="btn btn-outline-dark mt-2">Read More <i class="fa fa-arrow-right" aria-hidden="true"></i>
@@ -83,7 +83,7 @@ const Blog = () => {
               <img src={JavaScript} className="card-img-top" alt="10 things I learned about JavaScript" />
               <div className="card-body">
                 <h5 className="card-title">10 things I learned about JavaScript</h5>
-                <p className="text-muted"><small> <i class="fa fa-user" aria-hidden="true"></i> Subreena  |  <i class="fa fa-calendar" aria-hidden="true"></i> August 17 </small></p>
+                <p className="text-muted"><small> <i class="fa fa-user" aria-hidden="true"></i> Subreena  |  <i class="fa fa-calendar" aria-hidden="true"></i> August 17, 2020 </small></p>
                 <p className="card-text">Javascipt is a very hectic language to learn. It is very important for you if you want to be a web designer or a developer. I am still a learner a</p>
                 <a href="https://medium.com/@subreena20/10-things-i-learned-about-javascript-2746570fa8d5"
                  className="btn  btn-outline-dark mt-2">Read More <i class="fa fa-arrow-right" aria-hidden="true"></i>
